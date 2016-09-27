50c95a3608894b27d1b67b74c19e63b9623fd907
dgc <dgc@enservio.com>
2016-09-27 14:09:56 -0400

    Convert to using logback for zeidon logging

--- server/start_server.sh
+++ server/start_server.sh
@@ -4,7 +4,7 @@ JETTY_RUNNER=$(find jetty -name jetty-runner*)
 UTC_WAR=$(find build -name server*.war)
 PORT=8080
 
-java -jar $JETTY_RUNNER --port $PORT --classes $(pwd)/conf $UTC_WAR
+java -Xmx50m -DSQLITE_ROOT=$(pwd)/../sqlite -jar $JETTY_RUNNER --port $PORT --classes $(pwd)/conf $UTC_WAR
 
 exit 0
 
