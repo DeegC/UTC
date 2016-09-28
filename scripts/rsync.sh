f04f6453fc3923d548f2bfcfa98a9b40dd793c56
deegc <deegc@gitweb.com>
2016-09-27 23:41:00 -0400

    Added rsync.sh and install notes

---
+++ scripts/rsync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Rsync changes up to CHIP server.  This is faster than building it on CHIP.
+
+if [ "$1" == "" ]; then
+    echo "CHIP's IP address required as first param"
+    exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+DIR=$(pwd)
+echo $DIR
+rsync -av -v . $1:/home/dgc/projects/UTC
