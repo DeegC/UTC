f339f4e7933202e7572ec9630c44350279d1142f
deegc <deegc@gitweb.com>
2016-09-29 00:56:36 -0400

    Update activate.sh to use full qual oi

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 #curl -G --data-urlencode 'qual={"oi": {"id":10}}' localhost:4567/
-curl -G --data-urlencode 'qual={"oi": {"id":10}}' $UTC_SERVER:8080/utc/Configuration
+curl -G --data-urlencode 'qual={
+  "EntitySpec" : [ {
+    "EntityName" : "Configuration",
+    "QualAttrib" : [ {
+      "EntityName" : "Configuration",
+      "AttributeName" : "Id",
+      "Value" : "1",
+      "Oper" : "="
+    } ]
+  } ]
+}' $UTC_SERVER:8080/utc/Configuration
