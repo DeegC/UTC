0f3e836b7941d2ae62355eb0aff163485e43edf4
dgc <dgc@enservio.com>
2017-03-09 17:45:44 -0500

    Initialize PWM

--- install-notes/dev-setup.sh
+++ install-notes/dev-setup.sh
@@ -2,12 +2,17 @@
 
 # Used to set up dev environment for UTC.  Run on the CHIP as root.
 
-if id -u dgc; then
+# Change following to be whatever user you want to use.
+user=dgc
+
+reboot=0
+
+if id -u $user; then
     echo "User exists"
 else
-    adduser dgc
-    usermod -aG sudo dgc
-    echo "AllowUsers dgc" >> /etc/ssh/sshd_config
+    adduser $user
+    usermod -aG sudo $user
+    echo "AllowUsers $user" >> /etc/ssh/sshd_config
     service ssh reload
 fi
 
@@ -31,7 +36,7 @@ if ! which node; then
     curl -sL https://deb.nodesource.com/setup_7.x | bash -
 fi
 
-apt-get install ssmtp mailutils vim oracle-java8-installer git x11vnc emacs ruby htop wireless-tools screen gcc make nodejs
+apt-get install ssmtp mailutils vim oracle-java8-installer git x11vnc emacs ruby htop wireless-tools screen gcc make nodejs device-tree-compiler
 
 # Install file into device-tree to enable SPI support in the CHIP
 # Add lines to /etc/rc.local if they don't currently exist.
@@ -46,6 +51,15 @@ exit 0
 " >> /etc/rc.local
 fi
 
+# Set up PWM.  Pulled from https://gist.github.com/ugate/392185a959737365834f54d5bf4aae5b
+if ! find /sys/class/pwm/pwmchip0 2> /dev/null; then
+    cp /boot/sun5i-r8-chip.dtb /boot/sun5i-r8-chip.dtb.bak.$(date -d "today" +"%Y%m%d%H%M")
+    fdtput -t s /boot/sun5i-r8-chip.dtb "/soc@01c00000/pwm@01c20e00" "status" "okay"
+    fdtput -t s /boot/sun5i-r8-chip.dtb "/soc@01c00000/pwm@01c20e00" "pinctrl-names" "default"
+    fdtput -t x /boot/sun5i-r8-chip.dtb "/soc@01c00000/pwm@01c20e00" "pinctrl-0" "0x67" # 0x63 for older v4.4
+    reboot=1
+fi
+
 # Add a script to send an email with the local IP whenever a network connection is made.
 if [ ! -f /etc/network/if-up.d/send-ip ]; then
     echo "#!/bin/sh" > /etc/network/if-up.d/send-ip
@@ -54,8 +68,11 @@ if [ ! -f /etc/network/if-up.d/send-ip ]; then
 fi
 
 
-
 # Additional notes:
 
 # Run this if language isn't set properly:
 #sudo locale-gen en_US en_US.UTF-8 && sudo dpkg-reconfigure locales
+
+if $reboot; then
+    echo You must reboot the system.
+fi
