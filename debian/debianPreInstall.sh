1c701f2617c28c6f5e87849757d9d0ddbb8a65b0
deegc <deegc@gitweb.com>
2017-12-31 00:40:36 -0500

    Fix up debian install

--- debian/debianPreInstall.sh
+++ debian/debianPreInstall.sh
@@ -1,14 +1,25 @@
+#!/bin/sh
+
 user=utc
 
-adduser $user
+adduser --system $user
 adduser $user netdev
 
-# RPi groups
-adduser $user gpio
-adduser $user i2c
-adduser $user spi
+# For now we'll just assume we're on a RPi.  In the future we'll use 'uname -a' to determine
+# what env we're on.
+arch='rpi'
 
-if [ "$arch" == "chip" ]; then
+if [ "$arch" = "rpi" ]; then
+    # RPi groups
+    adduser $user gpio
+    adduser $user i2c
+    adduser $user spi
+fi
+
+if [ "$arch" = "chip" ]; then
     # On CHIP the user must have sudo authority.
     usermod -aG sudo $user
 fi
+
+# Let java apps access port 80
+sudo setcap CAP_NET_BIND_SERVICE=+eip $(readlink -f $(which java))
