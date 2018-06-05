95440ee153e847c193a47b504c1af52e59e9bbb2
deegc <deegc@gitweb.com>
2018-06-04 21:22:53 -0400

    Simplified test

--- bin/test/test.py
+++ bin/test/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import math
+import pdb
 
 SCLK        = 11 # Serial-Clock                                                                 
 MOSI        = 10 # Master-Out-Slave-In                                                  
@@ -31,53 +32,47 @@ def read_sensors():
     return values
 
 
-def temperatur_sensor (Rt, typ, unit): #Ermittelt die Temperatur                                                                                                
+def temperatur_sensor (Rt, typ, unit): #Ermittelt die Temperatur
+
+    if Rt == 0.0:
+        return -1.0
+    
     name = "ET-73" # Config_Sensor.get(typ,'name')
 
-    T = None
+    a = 0.00335672 # Config_Sensor.getfloat(typ,'a')
+    b = 0.000291888 # Config_Sensor.getfloat(typ,'b')
+    c = 0.00000439054 # Config_Sensor.getfloat(typ,'c')
+    Rn = 200 # Config_Sensor.getfloat(typ,'Rn')
 
-    if not name in ('PT100', 'PT1000'):
-        a = 0.00335672 # Config_Sensor.getfloat(typ,'a')
-        b = 0.000291888 # Config_Sensor.getfloat(typ,'b')
-        c = 0.00000439054 # Config_Sensor.getfloat(typ,'c')
-        Rn = 200 # Config_Sensor.getfloat(typ,'Rn')
+    v = math.log(Rt/Rn)
+    T = (1/(a + b*v + c*v*v)) - 273
 
-        try:
-            v = math.log(Rt/Rn)
-            T = (1/(a + b*v + c*v*v)) - 273
-        except: #bei unsinnigen Werten (z.B. ein- ausstecken des Sensors im Betrieb) Wert 999.9                                                                 
-            pass
-    else:
-        Rkomp = Config_Sensor.getfloat(typ,'Rkomp')
-        Rt = Rt - Rkomp
-        if name == 'PT100':
-            Rpt=0.1
-        else:
-            Rpt=1
-        try:
-            T = (-1)*math.sqrt( Rt/(Rpt*-0.0000005775) + (0.0039083**2)/(4*((-0.0000005775)**2)) - 1/(-0.0000005775)) - 0.0039083/(2*-0.0000005775)
-        except:
-            pass
-
-    if T is not None:
-        if unit == 'celsius':
-            return T
-        elif unit == 'fahrenheit':
-            return T * 1.8 +32
-
+    if T is None:
+        return -1.0
+    
+    if unit == 'celsius':
+        return T
+    elif unit == 'fahrenheit':
+        return T * 1.8 +32
 
 messwiderstand = 10
 maxadc = 4096.0
 
-values = read_sensors()
+#values = read_sensors()
+values =  [3908.0, 2837.0, 3914.0, 4095.0, 4095.0, 4095.0, 4095.0, 4095.0]
 print "ADC values = ", values
 
 for i in range(8):
     values[i] = 4095.0 - values[i]
-    values[i] = median_filter( [ values[i], values[i], values[i] ] )
+    #values[i] = median_filter( [ values[i], values[i], values[i] ] )
+
+for i in range(8):
     if values[i] > 0:
         values[i] = messwiderstand*((maxadc/ values[i]) - 1)
-        print "Value ", values[i], i
+
+print "Before SH values = ", values
+
+for i in range(8):
         values[i] = round(temperatur_sensor(values[i], 5, 'fahrenheit'), 2)
 
 print "Temperatures F = ", values
